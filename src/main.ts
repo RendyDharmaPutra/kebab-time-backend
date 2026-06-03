fcd7b1d348212e840c59a9cae3e41a63704b9d85
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-06-03 23:30:38 +0700

    feat: add global validation pipe with class-validator and class-transformer configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+
+      exceptionFactory: (errors) => {
+        return new BadRequestException({
+          code: 'VALIDATION_ERROR',
+          errors: errors.map((error) => ({
+            field: error.property,
+            messages: Object.values(error.constraints ?? {}),
+          })),
+        });
+      },
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
